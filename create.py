554fffcc874a4808e5d87a3a8d264562ac482273
Appuchia <46938448+appuchias@users.noreply.github.com>
2021-08-08 00:13:19 +0200

    Change python and python scripts folders

--- create.py
+++ create.py
@@ -46,9 +46,9 @@ LOCAL = pathlib.Path(__file__).parent.resolve()
 SCRIPTS = getenv("SCRIPTS_FOLDER_NAME")
 
 if args.script and not args.python:  # Set the new root destination to
-    DEST = path.join(DEST, SCRIPTS)
-elif args.script and args.python:
     DEST = path.join(DEST, "SCRIPTS")
+elif args.script and args.python:
+    DEST = path.join(DEST, SCRIPTS)
 
 NEWFOLDER = path.join(DEST, PROJECTNAME)  # New folder on destination
 
